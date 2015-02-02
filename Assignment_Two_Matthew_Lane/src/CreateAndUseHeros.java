771533a650493fed522fddb8a2741566313bbbf4
Matthew Lane <malane@rogers.com>
2015-02-02 13:38:43 -0500

    Added version History, added if else to catch the same power being applied more than once. Added more comments. Finished project!

--- Assignment_Two_Matthew_Lane/src/CreateAndUseHeros.java
+++ Assignment_Two_Matthew_Lane/src/CreateAndUseHeros.java
@@ -4,7 +4,12 @@
  * @author Matthew Lane (200214586)
  * @date 02/2/2015
  * @description Assignment 2, The Super Hero
- * @version 3.0
+ * 
+ * --History--
+ * @version 1.0 - Began this assignment. I used the Hero class from my previous assignment as the super class.
+ * @version 2.0 - Added necessary arrays, and generate abilities function.
+ * @version 3.0 - Fixed errors, learned a lesson about the different between java arrays, and c++ arrays.
+ * @version 4.0 - Added more comments, added version history, added if else to catch the same power being applied more than once Finished Assignment Two!
  */
 
 public class CreateAndUseHeros {
@@ -12,9 +17,10 @@ public class CreateAndUseHeros {
 	//The main method for the assignment
 	public static void main(String[] args) {
 		
-		//Create the Hero
+		//Create the SuperHero
 		SuperHero hero = new SuperHero("Smokey");
 		
-		
+		//Make the hero fight for our amusement.
+		hero.fight();
 	}
 }
--- Assignment_Two_Matthew_Lane/src/SuperHero.java
+++ Assignment_Two_Matthew_Lane/src/SuperHero.java
@@ -4,18 +4,22 @@
  * @author Matthew Lane (200214586)
  * @date 02/2/2015
  * @description Assignment 2, The Super Hero
- * @version 3.0
+ * 
+ * --History--
+ * @version 1.0 - Began this assignment. I used the Hero class from my previous assignment as the super class.
+ * @version 2.0 - Added necessary arrays, and generate abilities function.
+ * @version 3.0 - Fixed errors, learned a lesson about the different between java arrays, and c++ arrays.
+ * @version 4.0 - Added more comments, added version history, added if else to catch the same power being applied more than once Finished Assignment Two!
  */
 
 //This class is the SuperHero sub class. Its parent, is the Hero class. It allows further skills and abilities for specialized Heros.
 public class SuperHero extends Hero {
 
 	//Forward Declarations
-	
 	//The string array to hold the three random powers each super hero has. 
 	private String[] superPowers = new String[3];
 	
-	//Constructor ---------------
+	//Constructor --------------------
 	public SuperHero(String name) {
 		super(name);
 		
@@ -24,7 +28,7 @@ public class SuperHero extends Hero {
 		
 		//Display the Heros Powers
 		showPowers();
-	}
+	}//End of Constructor -----------
 	
 	//Function Generate Random Powers (Selects powers randomly from a string array and assigns them to the current Super Hero.
 	private void generateRandomPowers(){
@@ -32,14 +36,18 @@ public class SuperHero extends Hero {
 		//Array of possible powers to choose from
 		String[] possiblePowers = {"Super Speed", "Super Strength", "Body Armour", "Flight", "Fire Generation", "Weather Control"};
 		
-		
 		//Loop to assign random powers from the possiblePowers array to the superPowers array.
 		for (int powerGenerator = 0; powerGenerator < 3; powerGenerator ++)
-		{
+		{//Select a random number, to be used to select a random location in the superPowers array.
 			int powerSelected = (int)(Math.random() * 5);
+			
+			//If / Else to stop the same power being applied more than once. If it has been used, roll back the loop by one so we try again.
+			if (superPowers[0] == possiblePowers[powerSelected] || superPowers[1] == possiblePowers[powerSelected] || superPowers[2] == possiblePowers[powerSelected])
+			powerGenerator --;
+				
+			else
 			superPowers[powerGenerator] = possiblePowers[powerSelected];
 		}
-		
 	}
 	
 	//Function to display the heros super powers to the console. 
